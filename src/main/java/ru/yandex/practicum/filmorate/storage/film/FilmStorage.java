7b02255916ef375fc2dfdeeb1febda7371e44ef4
vkruglikov <vkrugman@yandex.ru>
2023-04-07 02:51:13 +0300

    provided codstile

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,7 +6,9 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Film create(Film film);
+
     Film update(Film film);
+
     Film delete(Film film);
 
     Collection<Film> getAllFilms();
